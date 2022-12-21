df5fdd747aa514caa5fab3acd306446974330ad8
LAPTOP-QR73DSMV\agent <merkulovwork@mail.ru>
2022-12-21 12:37:46 +1000

    ввод статей в БД

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', news_home, name='news_home')
+    path('', news_home, name='news_home'),
+    path('add/', news_adder, name='news_add'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 def news_home(request):
     news = Articles.objects.all()
     return render(request, 'news/news_home.html', {'news':news})
+
+
+def news_adder(request):
+    error = ''
+    if request.method == "POST":
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('main')
+        else:
+            error = 'Форма содержит ошибку'
+
+
+    form = ArticlesForm()
+
+    data = {
+        'form':form,
+        'error':error
+    }
+
+    return render(request, 'news/news_add.html', data)
